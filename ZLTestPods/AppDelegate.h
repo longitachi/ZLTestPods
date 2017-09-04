3e182675707052691bd7e2ead11e59fd7d434452
longitachi <longitachi@163.com>
2017-09-04 15:13:19 +0800

    Initial Commit

---
+++ ZLTestPods/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ZLTestPods
+//
+//  Created by long on 2017/9/4.
+//  Copyright © 2017年 long. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
